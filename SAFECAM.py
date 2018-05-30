44c4c41c22e1cb24e03a338a0709baa9831747af
Daniel Montecinos <danielmontecinoszambra@gmail.com>
2018-05-30 00:15:07 -0400

    loggin funcionando falta redireccionar a la ventana principal

--- SAFECAM.py
+++ SAFECAM.py
@@ -2,77 +2,127 @@
 import numpy as np
 import cv2
 import time
+import sys
+import cv2
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
-# Cargamos el vídeo
-camara = cv2.VideoCapture("detector-movimiento-opencv.mp4")
+from PyQt5.uic import loadUi
 
-# Inicializamos el primer frame a vacío.
-# Nos servirá para obtener el fondo
-fondo = None
 
-# Recorremos todos los frames
-while True:
-	# Obtenemos el frame
-	(grabbed, frame) = camara.read()
+class Persona:
+        def __init__(Nombre,Rut,Direccion,Telefono):
+                self.Nombre = Nombre
+                self.Rut = Rut
+                self.Direccion = Direccion
+                self.Telefono = Telefono
 
-	# Si hemos llegado al final del vídeo salimos
-	if not grabbed:
-		break
+class Encargado_de_seguridad(Persona):
+    def __init__(Nombre,Rut,Direccion,Telefono,Horario):
+        Persona.__init__(Nombre,Rut,Direccion,Telefono)
+        self.Horario = Horario
 
-	# Convertimos a escala de grises
-	gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+class duenho(Persona):
+    def __init__(Nombre,Rut,Direccion,Telefono,DireccionComercial):
+        Persona.__init__(Nombre,Rut,Direccion,Telefono)
+        self.DireccionComercial = DireccionComercial
 
-	# Aplicamos suavizado para eliminar ruido
-	gris = cv2.GaussianBlur(gris, (21, 21), 0)
+class SafeCam(QDialog):
+    def __init__(self):
+        super(SafeCam,self).__init__()
+##      cargo la platilla
+        loadUi('Plantilla/base.ui',self)
 
-	# Si todavía no hemos obtenido el fondo, lo obtenemos
-	# Será el primer frame que obtengamos
-	if fondo is None:
-		fondo = gris
-		continue
+class Login(QDialog):
+    def __init__(self):
+        self.usuario = "none"
+        self.contrasenha = "none"
+        super(Login,self).__init__()
+##      cargo la platilla
+        loadUi('Plantilla/loggin.ui',self)
+##      imagen inicial
+        self.Boton_Acceder.clicked.connect(self.onclickBoton_Acceder)
+#Cuando presionamos el boton de acceder, guardamos y comprobamos si existe el usuario
+    def onclickBoton_Acceder(self):
+        self.usuario = self.Usuario.text()
+        self.contrasenha = self.Contrasenha.text()
+        #Verificar_Usuario(self.usuario,self.contrasenha)
+        Verificar_Usuario(self.usuario,self.contrasenha,self.Label_Error)
 
-	# Calculo de la diferencia entre el fondo y el frame actual
-	resta = cv2.absdiff(fondo, gris)
+def Verificar_Usuario(usuario,contrasenha,Label_Error):
+    archivo = open("Usuarios/Usuarios.txt", "r")
+    #List [0]usuario [1] contrasenha
+    usuarios = archivo.readlines()
+    for i in usuarios:
+        i = i.rstrip('\n')
+        i = i.split(';')
+        aux_usuario = i[0]
+        aux_contrasenha = i[1]
+        if(aux_usuario == usuario and aux_contrasenha == contrasenha):
+            print("loggeado")
+            # app = QApplication(sys.argv)
+            # window_Base = SafeCam()
+            # window_Base.setWindowTitle('SafeCam')
+            # window_Base.show()
+            # sys.exit(app.exec_())
+            return 1
+        else:
+            Label_Error.setText('Usuario o contrasenha invalidos.')
+            return 0
+def save_webcam(outPath,fps,mirror=False):
+    # Capturing video from webcam:
+    cap = cv2.VideoCapture(0)
 
-	# Aplicamos un umbral
-	umbral = cv2.threshold(resta, 25, 255, cv2.THRESH_BINARY)[1]
+    currentFrame = 0
 
-	# Dilatamos el umbral para tapar agujeros
-	umbral = cv2.dilate(umbral, None, iterations=2)
+    # Get current width of frame
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+    # Get current height of frame
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
 
-	# Copiamos el umbral para detectar los contornos
-	contornosimg = umbral.copy()
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter(outPath, fourcc, fps, (int(width), int(height)))
 
-	# Buscamos contorno en la imagen
-	im, contornos, hierarchy = cv2.findContours(contornosimg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    while (cap.isOpened()):
 
-	# Recorremos todos los contornos encontrados
-	for c in contornos:
-		# Eliminamos los contornos más pequeños
-		if cv2.contourArea(c) < 500:
-			continue
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-		# Obtenemos el bounds del contorno, el rectángulo mayor que engloba al contorno
-		(x, y, w, h) = cv2.boundingRect(c)
-		# Dibujamos el rectángulo del bounds
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if ret == True:
+            if mirror == True:
+                # Mirror the output video frame
+                frame = cv2.flip(frame, 1)
+            # Saves for video
+            out.write(frame)
 
-	# Mostramos las imágenes de la cámara, el umbral y la resta
-	cv2.imshow("Camara", frame)
-	cv2.imshow("Umbral", umbral)
-	cv2.imshow("Resta", resta)
-	cv2.imshow("Contorno", contornosimg)
+            # Display el frame resultante
+            cv2.imshow('frame', frame)
+        else:
+            break
+# SI APRETA Q LO CIERRA
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-	# Capturamos una tecla para salir
-	key = cv2.waitKey(1) & 0xFF
+        # To stop duplicate images
+        currentFrame += 1
 
-	# Tiempo de espera para que se vea bien
-	time.sleep(0.015)
+    # When everything done, release the capture
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
-	# Si ha pulsado la letra s, salimos
-	if key == ord("s"):
-		break
+def main():
+    app = QApplication(sys.argv)
+    window_Login = Login()
+    window_Login.setWindowTitle('SafeCam')
+    window_Login.show()
+    sys.exit(app.exec_())
+    Fecha = time.strftime("%d-%m-%y")
 
-# Liberamos la cámara y cerramos todas las ventanas
-camara.release()
-cv2.destroyAllWindows()
+    #save_webcam('RegistroVideo/'+Fecha+'.avi', 30.0,mirror=True)
+
+
+if __name__ == '__main__':
+    main()
